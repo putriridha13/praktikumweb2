b5681aa92c32e07c72243de7714257317cca16f2
putriridha13 <putriridha01325@gmail.com>
2022-03-21 19:25:43 +0800

    modul1

--- modul1/PRAK101.php
+++ modul1/PRAK101.php
@@ -1,5 +1,5 @@
 <?php
     echo "Hello World in PHP <br>";
-    echo "Nama Praktikan: Putri Ridha Amalia <br>";
+    echo "Nama Praktikan : Putri Ridha Amalia <br>";
     echo "NIM: 2010817120007 <br>";
 ?>

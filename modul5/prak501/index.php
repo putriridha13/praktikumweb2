e41fad500f885e2b691b6aa08e87343d16756226
putriridha13 <101452713+putriridha13@users.noreply.github.com>
2022-05-14 20:44:23 +0800

    prak501

---
+++ modul5/prak501/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kumpulan Data</title>
+</head>
+
+<body>
+    <h1>Daftar Data</h1>
+    <ul>
+        <li>
+            <a href="Buku.php">Data Buku</a>
+        </li>
+        <li>
+            <a href="Member.php">Data Member</a>
+        </li>
+        <li>
+            <a href="Peminjaman.php">Data Peminjaman</a>
+        </li>
+    </ul>
+</body>
+
+</html>

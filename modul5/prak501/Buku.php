5d2f7663f11914cbeb4ce519f358b56dc330a314
putriridha13 <101452713+putriridha13@users.noreply.github.com>
2022-05-14 20:42:08 +0800

    e

---
+++ modul5/prak501/Buku.php
@@ -0,0 +1,39 @@
+<?php
+require('./Model.php');
+if (isset($_GET['id_buku'])) {
+    hapusbuku($_GET['id_buku']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Buku</title>
+    <style>
+    th {
+        background-color: #D3D3D3;
+    }
+    </style>
+</head>
+
+<body>
+    <h1>Data Buku</h1>
+    <a href="FormBuku.php"><button style='background-color:green; color:white'>Tambah Data</button></a>
+    <table border="1" cellpadding='10'>
+        <tr>
+            <th>ID Buku</th>
+            <th>Judul Buku</th>
+            <th>Penulis</th>
+            <th>Penerbit</th>
+            <th>Tahun Terbit</th>
+            <th>Aksi</th>
+        </tr>
+        <?= tampil("buku") ?>
+    </table>
+</body>
+
+</html>
